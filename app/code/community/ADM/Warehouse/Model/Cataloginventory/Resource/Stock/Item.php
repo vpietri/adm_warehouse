fa8e858aba930a6e28f06c4cd0812ec7c1bf0da6
Vincent Pietri <vincent.pietri@gmail.com>
2017-04-27 12:11:30 +0200

    Fix bug on admin catalog/product/view/associated products tab

--- app/code/community/ADM/Warehouse/Model/Cataloginventory/Resource/Stock/Item.php
+++ app/code/community/ADM/Warehouse/Model/Cataloginventory/Resource/Stock/Item.php
@@ -46,4 +46,36 @@ class ADM_Warehouse_Model_CatalogInventory_Resource_Stock_Item extends Mage_Cata
         $this->_afterLoad($item);
         return $this;
     }
+
+
+    /**
+     * Add join for catalog in stock field to product collection
+     *
+     * @param Mage_Catalog_Model_Resource_Product_Collection $productCollection
+     * @return Mage_CatalogInventory_Model_Resource_Stock_Item
+     */
+    public function addCatalogInventoryToProductCollection($productCollection)
+    {
+        $adapter = $this->_getReadAdapter();
+        $isManageStock = (int)Mage::getStoreConfig(Mage_CatalogInventory_Model_Stock_Item::XML_PATH_MANAGE_STOCK);
+        $stockExpr = $adapter->getCheckSql('sub_cisi.use_config_manage_stock = 1', $isManageStock, 'sub_cisi.manage_stock');
+        $stockExpr = $adapter->getCheckSql("({$stockExpr} = 1)", 'sub_cisi.is_in_stock', '1');
+
+        $subSelect = $adapter->select()->from(array('sub_cisi' => $productCollection->getTable('cataloginventory/stock_item')),
+                array(
+                'product_id',
+                'is_saleable' => new Zend_Db_Expr('MAX('.$stockExpr.')'),
+                'inventory_in_stock' => new Zend_Db_Expr('MAX(is_in_stock)')
+            ))->group('product_id');
+
+        $subquery = new Zend_Db_Expr('(' . $subSelect->__toString() .')');
+        $productCollection->joinTable(array('cisi' => $subquery),
+                'product_id=entity_id',
+                array('is_saleable','inventory_in_stock'),
+                    null,
+                    'left'
+                );
+
+        return $this;
+    }
 }
